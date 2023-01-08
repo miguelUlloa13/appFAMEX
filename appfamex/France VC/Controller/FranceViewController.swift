e7fc927b6a52a9054705296ad2b817756ae311bc
Miguel <miguel.bmangelulloa13@gmail.com>
2023-01-08 17:31:42 -0600

    France storyboard done

---
+++ appfamex/France VC/Controller/FranceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FranceViewController.swift
+//  appfamex
+//
+//  Created by Miguel Angel Bric Ulloa on 08/01/23.
+//
+
+import UIKit
+
+class FranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
